a9a7ae19111e66cde824f41c0f5c8044296305e4
akif <70863898+akif-ned@users.noreply.github.com>
2023-06-22 11:38:46 +0200

    first commit

---
+++ lessons/lesson1.js
@@ -0,0 +1,24 @@
+//1. hello world
+console.log("Hello world")
+// Variables
+var firstName="Akif"
+let lastName="Yorulmaz"
+console.log(firstName)
+var age, dateOfBirth, bestNumber
+age= 13
+dateOfBirth=2010
+
+// we can override the variables
+age= 15
+console.log(age)
+//constans
+const occupation="Test Engineer"
+console.log(occupation)
+//data types
+var name="Akif"// string it is a just a text
+var ageOfBrother=19// number it is  just a number
+var isHeMarried=false// boolean
+
+var yearsOfMarriages=null// just no value
+
+var numberOfCars=undefined// 
---
+++ lessons/lesson3.js
@@ -0,0 +1,12 @@
+// objects
+var customer ={
+    firtName:'Akif',
+    lastName:'Yorulmaz'
+}
+//dot notation
+console.log(customer.firtName)
+console.log(customer.lastName)
+// we can assign new value to our object fields
+customer.firtName="King Akif"
+console.log(customer.firtName)
+//arrays
